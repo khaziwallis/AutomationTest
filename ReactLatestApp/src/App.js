9b76b95be8eb1a1cec70cdfd002ea4294460fc32
Vinay-Sunny <vinaysunny9@accionlabs.com>
2018-11-05 10:39:00 +0530

    initial-react-latest-app-changes

--- ReactLatestApp/src/App.js
+++ ReactLatestApp/src/App.js
@@ -6,37 +6,6 @@ import LoginPage from "./components/login";
 import Home from "./components/home";
 import Error from "./components/error";
 
-// const checkAuth = () => {
-//   const token = localStorage.getItem("token");
-//   const refreshToken = localStorage.getItem("refreshToken");
-//   if (!token || !refreshToken) {
-//     return false;
-//   }
-//   try {
-//     const { exp } = decode(refreshToken);
-//     if (exp < new Date().getTime() / 1000) {
-//       return false;
-//     }
-//   } catch (e) {
-//     return false;
-//   }
-
-//   return true;
-// };
-
-// const AuthRoute = ({ component: Component, ...rest }) => (
-//   <Route
-//     {...rest}
-//     render={props =>
-//       checkAuth() ? (
-//         <Component {...props} />
-//       ) : (
-//         <Redirect to={{ pathname: "/" }} />
-//       )
-//     }
-//   />
-// );
-
 class App extends Component {
   render() {
     return (
