c2060dce758f4d84dd9741ab925a591184f8b872
shanth21 <shanth.gaitonde@accionlabs.com>
2018-12-04 17:07:11 +0530

    demo test-hook

--- TestFramework/stories/test.spec.js
+++ TestFramework/stories/test.spec.js
@@ -4,21 +4,13 @@ describe('Test started...', function () {
 	var testSuites = [{
         url: 'http://localhost:9000',
         expectedTests: [{
-            testHook: 'title-label',
-            message: 'should have expected title label',
-            expected: 'Title:',
+            testHook: 'userNameLbl',
+            message: 'should have expected user name label',
+            expected: 'Username',
         }, {
-            testHook: 'title-value',
-            message: 'should have expected title',
-            expected: 'Test Title',
-        }, {
-            testHook: 'description-label',
-            message: 'should have expected description label',
-            expected: 'Description:',
-        }, {
-            testHook: 'description-value',
-            message: 'should have expected description',
-            expected: 'test description',
+            testHook: 'passwordLbl',
+            message: 'should have expected pawwsord label',
+            expected: 'Password',
         }]
     }];
 
